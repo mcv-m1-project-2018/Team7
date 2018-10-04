e4adb13ce1fce9b65159ded88b8eec7be3bada83
Marc Perez <marc.perez@idiada.com>
2018-10-04 21:01:09 +0200

    added analysis of blue color segmentation based on hue value

--- color_analysis.py
+++ color_analysis.py
@@ -18,6 +18,57 @@ def parse_sign_type(sign_type):
     elif sign_type == 'F':
         return "Blue Square"
 
+def red_test(gt, mask_file_path, image_file_path):
+    for ann in gt:
+        image = cv2.imread(image_file_path + ann[0] + ".jpg")
+        mask_im = cv2.imread(mask_file_path + "mask." + ann[0] + ".png", 0)
+
+        cv2.imshow('image', image)
+        cv2.waitKey(0)
+        cv2.imshow('mask', mask_im * 255)
+        cv2.waitKey(0)
+
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        red_1_high = np.array((5, 255, 255), dtype = 'uint8')
+        red_1_low = np.array((0, 50, 50), dtype = 'uint8')
+        red_2_low = (175, 50, 50)
+        red_2_high = (180, 255, 255)
+
+        mask_hue_red_1 = cv2.inRange(hsv_image, red_1_low, red_1_high)
+        mask_hue_red_2 = cv2.inRange(hsv_image, red_2_low, red_2_high)
+        combined_red_mask = cv2.bitwise_or(mask_hue_red_1, mask_hue_red_2)
+
+        cv2.imshow("mask_red", mask_hue_red_1)
+        cv2.waitKey(0)
+        cv2.imshow("mask_red", mask_hue_red_2)
+        cv2.waitKey(0)
+        cv2.imshow("mask_red", combined_red_mask)
+        cv2.waitKey(0)
+
+def blue_test(gt, mask_file_path, image_file_path):
+    for ann in gt:
+        image = cv2.imread(image_file_path + ann[0] + ".jpg")
+        mask_im = cv2.imread(mask_file_path + "mask." + ann[0] + ".png", 0)
+
+        cv2.imshow('image', image)
+        cv2.waitKey(0)
+        cv2.imshow('mask', mask_im * 255)
+        cv2.waitKey(0)
+
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        blue_low = np.array((105, 30, 30), dtype='uint8')
+        blue_high = np.array((135, 255, 255), dtype='uint8')
+
+
+        mask_hue_blue = cv2.inRange(hsv_image, blue_low, blue_high)
+
+        cv2.imshow("mask_blue", mask_hue_blue)
+        cv2.waitKey(0)
+
+
+
 def color_analysis(gt, mask_file_path, image_file_path):
     """
     This function extracts information about the color.
@@ -30,38 +81,15 @@ def color_analysis(gt, mask_file_path, image_file_path):
     for ann in gt:
         image = cv2.imread(image_file_path + ann[0] + ".jpg")
         mask_im = cv2.imread(mask_file_path + "mask." + ann[0] + ".png", 0)
-        cv2.imshow('image', image)
-        cv2.waitKey(0)
-        cv2.imshow('mask', mask_im * 255)
-        cv2.waitKey(0)
-        """cv2.imshow('image', image[:,:,0])
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        cv2.imshow('image', image[:, :, 1])
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        cv2.imshow('image', image[:, :, 2])
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
-        colors = ('b', 'g', 'r')
+        """colors = ('b', 'g', 'r')
         for i, color in enumerate(colors):
             hist = cv2.calcHist([image], [i], mask_im, [256], [0, 256])
             plt.plot(hist, color=color)
-        plt.show()
-        """
+        plt.show()"""
+
         hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        print("hey")
-        red_1_high = np.array((2, 255, 255), dtype = 'uint8')
-        red_1_low = np.array((0, 50, 50), dtype = 'uint8')
-        red_2_high = (170, 255, 255)
-        red_2_low = (180, 50, 50)
-        print(type(hsv_image))
-        mask_hue_red_1 = cv2.inRange(hsv_image, red_1_low, red_1_high)
-        print (mask_hue_red_1)
-        print(type(mask_hue_red_1))
-        cv2.imshow("red_mask", mask_hue_red_1)
-        cv2.waitKey(0)
+
         """cv2.imshow('image H', hsv_image[:, :, 0])
         cv2.waitKey(0)
         cv2.destroyAllWindows()
@@ -71,7 +99,7 @@ def color_analysis(gt, mask_file_path, image_file_path):
         cv2.imshow('image V', hsv_image[:, :, 2])
         cv2.waitKey(0)
         cv2.destroyAllWindows()"""
-    """"
+
         histH = cv2.calcHist([hsv_image], [0], mask_im, [256], [0, 256])
         histS = cv2.calcHist([hsv_image], [1], mask_im, [256], [0, 256])
         histV = cv2.calcHist([hsv_image], [2], mask_im, [256], [0, 256])
@@ -96,10 +124,6 @@ def color_analysis(gt, mask_file_path, image_file_path):
 
         plt.title(sign_type+": "+parse_sign_type(sign_type))
         plt.show()
-    """
-
-def generate_mask_hue_range(image, start, end):
-    return image[:,:,0] >= start and image[:,:,0] >= end
 
 
 def main():
@@ -108,7 +132,9 @@ def main():
     mask_file_path = "./train/mask/"
 
     gt = read_gt(gt_file_path)
-    color_analysis(gt, mask_file_path=mask_file_path, image_file_path=image_file_path)
+    #color_analysis(gt, mask_file_path=mask_file_path, image_file_path=image_file_path)
+    #red_test(gt, mask_file_path=mask_file_path, image_file_path=image_file_path)
+    blue_test(gt, mask_file_path=mask_file_path, image_file_path=image_file_path)
 
 
 if __name__ == "__main__":
