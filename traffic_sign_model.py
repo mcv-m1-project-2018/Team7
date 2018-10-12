89e987855cba58f260232363e313b795beca1016
Sergigb <sergi.garciab@e-campus.uab.cat>
2018-10-12 10:41:10 +0200

    new morphology: close+open+ccl+filling+dilatation+filling

--- traffic_sign_model.py
+++ traffic_sign_model.py
@@ -5,24 +5,25 @@ import cv2
 from data import Data_handler
 from data_analysis import Data_analysis
 from traffic_signs import traffic_sign_detection as detection
+import matplotlib.pyplot as plt
 
 
 
 class Traffic_sign_model():
     def __init__(self):
-        self.blue_low_hsv  = (105,   30, 30)
+        self.blue_low_hsv = (105, 30, 30)
         self.blue_high_hsv = (135, 255, 255)
-        self.red1_low_hsv  = (0,    50, 50)
-        self.red1_high_hsv = (7,   255, 255)
-        self.red2_low_hsv  = (177,   50, 50)
-        self.red2_high_hsv = (180,  255, 255)
+        self.red1_low_hsv = (0, 50, 50)
+        self.red1_high_hsv = (8, 255, 255)
+        self.red2_low_hsv = (177, 50, 50)
+        self.red2_high_hsv = (181, 255, 255)
 
     def pixel_method(self, im):
 
         hsv_image = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 
         blue_low   = np.array(self.blue_low_hsv, dtype='uint8')
-        blue_high  = np.array(self.blue_high_hsv,    dtype='uint8')
+        blue_high  = np.array(self.blue_high_hsv, dtype='uint8')
         
         red_1_low  = np.array(self.red1_low_hsv, dtype='uint8')
         red_1_high = np.array(self.red1_high_hsv, dtype='uint8')
@@ -35,9 +36,16 @@ class Traffic_sign_model():
 
         combined_red_mask = cv2.bitwise_or(mask_hue_red_1, mask_hue_red_2)
         mask_hue_blue     = cv2.inRange(hsv_image, blue_low, blue_high)
-        final_mask        = cv2.bitwise_or(combined_red_mask, mask_hue_blue)        
+        final_mask        = cv2.bitwise_or(combined_red_mask, mask_hue_blue)
+
+        # f, axarr = plt.subplots(1, 2)
+        # axarr[0].imshow(final_mask)
 
         final_mask = self.morph_transformation(final_mask)
+
+        # axarr[1].imshow(final_mask)
+        # plt.show()
+
         return final_mask
 
 
@@ -49,21 +57,23 @@ class Traffic_sign_model():
         :return: more pixels
         """
 
-        # opening + closing to remove artifacts and holes.
-        kernel = np.ones((5, 5), np.uint8)
+        # kernel = np.ones((5, 5), np.uint8)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
         pixel_candidates = cv2.morphologyEx(pixel_candidates, cv2.MORPH_OPEN, kernel)
 
-        kernel = np.ones((10, 10), np.uint8)
+        # kernel = np.ones((10, 10), np.uint8)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
         pixel_candidates = cv2.morphologyEx(pixel_candidates, cv2.MORPH_CLOSE, kernel)
 
         # find all contours (segmented areas) of the mask to delete those that are not consistent with the train split
         # analysis
-        image, contours, hierarchy = cv2.findContours(pixel_candidates, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-        max_aspect_ratio = 1.419828704905269 * 1.15
-        min_aspect_ratio = 0.5513618362563639 * 0.85
-        max_area = 55919.045 * 1.10
+        max_aspect_ratio = 1.419828704905269 * 1.25
+        min_aspect_ratio = 0.5513618362563639 * 0.75
+        max_area = 55919.045 * 1.15
         min_area = 909.7550000000047 * 0.75
+
+        image, contours, hierarchy = cv2.findContours(pixel_candidates, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         for contour in contours:
             # max and min coordinates of the segmented area
@@ -81,11 +91,29 @@ class Traffic_sign_model():
             # colors) with cv2.fillPoly.
             if max_aspect_ratio > height/width > min_aspect_ratio and max_area > width*height > min_area:
                 cv2.fillPoly(pixel_candidates, pts=[contour], color=255)
-            # If the area is too big/small/wide/tall it gets erased from the mask.
             else:
-                for x in range(y_min-1, y_max+1):
-                    for y in range(x_min-1, x_max+1):
-                        pixel_candidates[x, y] = 0
+                cv2.fillPoly(pixel_candidates, pts=[contour], color=0)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+        pixel_candidates = cv2.dilate(pixel_candidates, kernel, iterations=1)
+
+        image, contours, hierarchy = cv2.findContours(pixel_candidates, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        for contour in contours:
+            xcnts = np.vstack(contour.reshape(-1, 2))
+            x_min = min(xcnts[:, 0])
+            x_max = max(xcnts[:, 0])
+            y_min = min(xcnts[:, 1])
+            y_max = max(xcnts[:, 1])
+
+            width = y_max - y_min
+            height = x_max - x_min
+
+            if max_aspect_ratio > height/width > min_aspect_ratio and max_area > width*height > min_area:
+                cv2.fillPoly(pixel_candidates, pts=[contour], color=255)
+            else:
+                cv2.fillPoly(pixel_candidates, pts=[contour], color=0)
+
 
         return pixel_candidates
 
@@ -116,17 +144,17 @@ def main(args):
     data_hdlr = Data_handler(train_dir=args.images_dir)
     data_hdlr.read_all()
 
-    print("analyzing the train split...\n")
-    sign_count, max_area, min_area, filling_ratios, max_aspect_ratio, min_aspect_ratio = Data_analysis.shape_analysis\
-        (data_hdlr.train_set)
+    # print("analyzing the train split...\n")
+    # sign_count, max_area, min_area, filling_ratios, max_aspect_ratio, min_aspect_ratio = Data_analysis.shape_analysis\
+    #     (data_hdlr.train_set)
 
     model = Traffic_sign_model()
-
-    for key in filling_ratios.keys():
-        print(key + ": " + str(filling_ratios[key]))
-
-    print("sign_count: ", sign_count, "\n", "max_area: ", max_area, "\n", "min_area: ", min_area,
-          "\n", "max_aspect_ratio: ", max_aspect_ratio, "\n", "min_aspect_ratio: ", min_aspect_ratio)
+    #
+    # for key in filling_ratios.keys():
+    #     print(key + ": " + str(filling_ratios[key]))
+    #
+    # print("sign_count: ", sign_count, "\n", "max_area: ", max_area, "\n", "min_area: ", min_area,
+    #       "\n", "max_aspect_ratio: ", max_aspect_ratio, "\n", "min_aspect_ratio: ", min_aspect_ratio)
 
     print("\nprocessing the val split...\n")
     pixel_precision, pixel_accuracy, pixel_specificity, pixel_sensitivity, window_precision, window_accuracy = \
@@ -135,10 +163,10 @@ def main(args):
 
     print(pixel_precision, pixel_accuracy, pixel_specificity, pixel_sensitivity, window_precision, window_accuracy)
 
-    print("\nprocessing the test split...")
-
-    detection.traffic_sign_detection("test", args.test_dir, data_hdlr.test_set, args.output_dir, 'hsvClosing',
-                                     model.pixel_method, args.windowMethod)
+    # print("\nprocessing the test split...")
+    #
+    # detection.traffic_sign_detection("test", args.test_dir, data_hdlr.test_set, args.output_dir, 'hsvClosing',
+    #                                  model.pixel_method, args.windowMethod)
 
 
 if __name__ == "__main__":
